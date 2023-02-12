e0f682271280549536881ed6a321f2ca94c9d67f
laohu <laohubbb@gmail.com>
2023-02-12 23:42:48 +0800

    增加指定时间功能

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,110 @@
 <script setup>
 import dayjs from 'dayjs'
-import {onMounted, ref} from "vue";
+import { onMounted, ref } from "vue";
+import Modal from './components/Modal.vue'
 
+let customHour = ref('23')
+let customMinute = ref('44')
 const today = dayjs().format('YYYY-MM-DD')
-const night = dayjs(today + ' 23:00:00')
-let hour=ref(0)
-let minute=ref(0)
 
+
+let hour = ref(0)
+let minute = ref(0)
+let visible = ref(false)
 onMounted(() => {
-  const computeDiff=()=>{
-    let now = dayjs()
-    hour.value = night.diff(now, 'hour')
-    minute.value = night.diff(now, 'minute') % 60
-    document.title=`${hour.value}h${minute.value}m`
-  }
+  getTime()
+
   computeDiff()
+
   setInterval(computeDiff, 10000)
 
 })
+const closeModal = () => {
+  localStorage.setItem('customHour', formatNumber(customHour.value))
+  localStorage.setItem('customMinute', formatNumber(customMinute.value))
+  computeDiff()
+  visible.value = false
+}
+const formatNumber = (input) => {
+  input = parseInt(input)
+  if (typeof input === "number") {
+    if (input >= 0 && input < 10) {
+      return "0" + input.toString();
+    } else {
+      return input.toString();
+    }
+  } else {
+    return "00";
+  }
+}
+const getTime = () => {
+  let hour = localStorage.getItem('customHour')
+  let minute = localStorage.getItem('customMinute')
+
+  if (hour && minute) {
+    customHour.value = hour
+    customMinute.value = minute
+  }
+
+}
+const computeDiff = () => {
+  let now = dayjs()
+  let customTime = dayjs(today + ` ${customHour.value}:${customMinute.value}:00`)
+  hour.value = customTime.diff(now, 'hour')
+  minute.value = customTime.diff(now, 'minute') % 60
+  document.title = `${hour.value}h${minute.value}m`
+}
 </script>
 
 <template>
 
-  <div class="box">
-    <div class="title">距离11点还有</div>
+  <div class="box" @click="visible = true">
+
     <div class="time">
+      <div class="title">距离指定时间还有</div>
       <span class="hour">{{ hour }}</span>
       小时
       <span class="minute">{{ minute }}</span>
       分钟
     </div>
   </div>
+  <div class="hello">
+
+    <modal v-if="visible" @close="closeModal">
+      <template v-slot:header>
+        <h3>自定义时间</h3>
+      </template>
+      <template v-slot:body>
+        <input type="number" v-model="customHour">:
+        <input type="number" v-model="customMinute">
+      </template>
+    </modal>
+  </div>
 </template>
 
 <style scoped>
+
 .box {
-  height: 100vh;
+  height: 99vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  position: relative;
 }
 
 .title {
-  font-size: 10vw;
+  font-size: 2vw;
   margin-bottom: 10px;
+  position: absolute;
+  top: -30px;
+  left: 0;
 }
 
 .time {
   font-size: 10vw;
+
+  position: relative;
 }
 
 .hour,
---
+++ src/components/Modal.vue
@@ -0,0 +1,93 @@
+<template>
+    <transition name="modal">
+        <div class="modal-mask">
+            <div class="modal-wrapper">
+                <div class="modal-container">
+                    <div class="modal-header">
+                        <slot name="header">
+                            default header
+                        </slot>
+                    </div>
+
+                    <div class="modal-body">
+                        <slot name="body">
+                            default body
+                        </slot>
+                    </div>
+
+                    <div class="modal-footer">
+                        <slot name="footer">
+                            <button class="modal-default-button" @click="$emit('close')">
+                                OK
+                            </button>
+                        </slot>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script lang="js">
+export default {
+    data() {
+        return {
+            show: false
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+.modal-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, .5);
+    display: table;
+    transition: opacity .3s ease;
+}
+
+.modal-wrapper {
+    display: table-cell;
+    vertical-align: middle;
+}
+
+.modal-container {
+    width: 500px;
+    margin: 0px auto;
+    padding: 20px 30px;
+    background-color: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+    transition: all .3s ease;
+    font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header {
+    padding: 20px 0;
+}
+
+.modal-body {
+    padding: 20px 0;
+}
+
+.modal-footer {
+    padding: 10px;
+    text-align: right;
+}
+
+.modal-default-button {
+    border-radius: 2px;
+    padding: 8px 14px;
+    background-color: #42b983;
+    border: 1px solid #42b983;
+    color: #fff;
+    font-size: 15px;
+    cursor: pointer;
+}
+</style>
